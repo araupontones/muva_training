620c0f6478c3a162afefd277338dac1da350a2af
nhanombedercio <nhanombe.benaldou@gmail.com>
2021-04-01 11:53:30 +0200

    Alteracao da base de dado para mapa - Actualização

--- R/mapa_leaflet_andres.R
+++ R/mapa_leaflet_andres.R
@@ -2,24 +2,17 @@ library(pacman)
 
 p_load(rio, tidyverse, janitor, leaflet, sf)
 
-list.files("maps/Data")
+list.files("Data/trenamento_mapas")
 
-dir_data_maps = "maps/Data"
+dir_data_maps = "Data/trenamento_mapas"
 
-#importar kml
 
-bairro_boundaries = st_read(file.path(dir_data_maps, "Bairro 25 de junho.kml"))
-test = st_transform(bairro_boundaries, 4326)
-st_bbox(test)
 #importar dados -----------------------------------------
 
-susamati_points_raw = import(file.path(dir_data_maps, "Cópia de Susamati.xlsx"))
+susamati_points_raw = import(file.path(dir_data_maps, "Base de dados 15 de Agosto.xlsx"))
 susamati_points = susamati_points_raw %>% 
-  select(nome,bairro, Longitude, Latitude, tipo_latrina) %>%
-  filter(tipo_latrina != ".a") %>%
-  filter(!nome %in% c("PEDRO FELIZARDA", "PINTO JOSINE", "ALFREDO ANTÓNIO",
-                      "FERNANDO GUAMBE", "FATIMA JOÃO BOA", "FERNANDO LOPES")) %>%
-  mutate(tipo_latrina = factor(tipo_latrina))
+        
+mutate(tipo_latrina = factor(tipo_latrina))
 # %>%
 #mutate(tipo_latrina = str_remove(tipo_latrina," ligad[a-z] a um buraco no chão" ))
 str(susamati_points)
@@ -35,8 +28,7 @@ unique(susamati_points$tipo_latrina)
 #####darkblue = "#1F2136"
 #####grey = "#cccccc"
 pal <- colorFactor(c("#5DD4C7", "#F77333", 'green', "maroon", 'orange', "black", "red", 'purple'),
- domain = susamati_points$tipo_latrina
-)
+ domain = susamati_points$tipo_latrina)
 
 susamati_points %>%
   janitor::tabyl(tipo_latrina)
@@ -45,19 +37,12 @@ view(susamati_points_raw)
 
 leaflet(data = susamati_points) %>%
   addTiles() %>%
-  leaflet::addCircleMarkers(label = ~tipo_latrina,
+  leaflet::addCircleMarkers(label = ~nome,
                             radius = 3,
                             color = ~pal(tipo_latrina)) %>%
   addLegend("topright", pal = pal, values =~tipo_latrina, title = "Tipo de Latrina")
   
-leaflet::addPolygons(data = test)
+ 
 
 
-leaflet() %>%
-  addPolygons(data = bairro_boundaries,
-              color = "#444444", weight = 1, smoothFactor = 0.5)
-
-geterrmessage()
-st_crs(bairro_boundaries)
-ggplot(data = bairro_boundaries)+
-  geom_sf()
+ 
